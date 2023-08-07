35f45cf1a9c1672b3b14b371933c0f18de015fd0
sanlangguo <SanLangGuo@outlook.com>
2023-08-07 10:27:35 +0800

    build: build docs

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-pnpm docs:build
+yarn build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -16,4 +16,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:sanlangguo/webpack_docs.git main:gh-pages
 
-cd - add 
+cd - add
